48d5283a2fb7890690ba80f5accf8739fef74c88
sofiyakogut <sofiakogut39@icloud.com>
2022-12-13 12:21:23 +0100

    :)

---
+++ modificacion1_1/src/cFilPrincipal/cFilPrincipal.java
@@ -0,0 +1,28 @@
+package cFilPrincipal;
+import cFil.cFil;
+public class cFilPrincipal {
+  public static void main (String [] pArguments){
+	  
+    System.out.println ("Fil principal iniciat.");
+    System.out.println("Fil secundari iniciat.");
+    
+    cFil vObjecteFil = new cFil("#1");
+    Thread vFil = new Thread(vObjecteFil);
+    vFil.start();
+    
+    System.out.println("Iniciant execució procés principal");
+	   	try {
+	      for(int vComptador = 0; vComptador < 10; vComptador++){
+	        Thread.sleep(500);
+	        System.out.println("Despertant aturada " + vComptador + " procès principal");
+	       
+	      }
+	    }
+	    catch(InterruptedException pExcepcio){
+	      System.out.println("Interrompent execució procès principal");
+	    }
+	    
+	System.out.println("Acabant execució procès principal");
+    
+  }
+}
